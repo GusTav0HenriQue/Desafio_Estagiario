2b4b2265a12b0a48a834242794e3a332b529e327
Gustavo <gustavo.henriqe@protechsolutions.com.br>
2022-09-09 13:58:31 -0300

    Add o Update user validator

---
+++ Services/Validator/User/UpdateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using Dominio.DTOs.UserDtos;
+using FluentValidation;
+
+namespace Service.Validator.User
+{
+    public class UpdateUserValidator :  AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(u => u.Nome).NotEmpty().WithMessage("O {PropertyName} não pode ser vazio nem nulo").Length(4, 30).WithMessage("O {PropertyName} não pode ser menor que {MinLength} ou maior que {MaxLength}");
+
+            RuleFor(u => u.Password).NotEmpty().WithMessage("O {PropertyName} não pode ser vazio nem  nulo")
+                .Length(5, 15).WithMessage("O {PropertyName} não pode ser menor que {MinLength} ou maior que {MaxLength}!");
+
+            RuleFor(u => u.RePassword).NotEmpty().WithMessage("O {PropertyName} não pode ser vazio nem  nulo")
+                .Length(5, 15).WithMessage("O {PropertyName} não pode ser menor que {MinLength} ou maior que {MaxLength}!");
+        }
+    }
+}
